9c8e7fafdc792ab7d42f8fe174a6719764f34e17
Fernando Bellido <fbellidopazos@gmail.com>
2018-11-24 23:02:41 +0100

    Update User.py

--- User.py
+++ User.py
@@ -3,13 +3,13 @@ import ast
 #Arrays de traduccion
 array={"a":"31","b":"32","c":"33","d":"34","e":"35","f":"36","g":"37","h":"38","i":"39","j":"10","k":"11","l":"12","m":"13","n":"14","o":"15","p":"16","q":"17","r":"18","s":"19","t":"20","u":"21","v":"22","w":"23","x":"24","y":"25","z":"26"," ":"27",".":"28"}
 array1 = {v: k for k, v in array.items()}
-
+encrypt=CryptoLib.encrypt()
 while(True):
     #Get all Keys
-    encrypt=CryptoLib.encrypt()
+
 
     #Menu
-    input1=str(input("Menu \n=================================================== \n1.-Encode \n2.-Decode \n3.-Exit \n=================================================== \nCommand: "))
+    input1=str(input("\nMenu \n=================================================== \n1.-Encode \n2.-Decode \n3.-Keys(Recommended after introducing other keys) \n4.-Exit \n=================================================== \nCommand: "))
     if(input1.lower()=="encode"):
         #Get Message and Translate into Array
         msg=str(input("Message to Encrypt: "))
@@ -34,7 +34,13 @@ while(True):
         #Prints Keys and Message
         print("Publica: "+str(encrypt[3])+"\n\nModulo: "+str(encrypt[2])+"\n\nPrimes: \n      P= " +str(encrypt[0])+"\n      Q= " +str(encrypt[1])+"\n\nPRIVADA: "+str(encrypt[4])+"\n\nMensaje: "+str(msg)+"          Mensaje Traducido: "+ str(msgtr) +"        Mesansaje Encryptado: "+ str(msg_encriptado))
     elif(input1.lower()=="decode"):
-
-        break
+        input3=str(input("Uncode \n===================================================\n1.-Normal \n2.-Debug \n=================================================== \nCommand: "))
+        input2=int(input("Message to Decode: "))
+        if(input3.lower()=="normal"):
+            encrypt[4]=int(input("Clave Privada: "))
+            encrypt[2]=int(input("Modulo: "))
+        print("The Coded Message: "+str(input2)+"\n\nDecoded: "+CryptoLib.dencrypt(input2,encrypt[4],encrypt[2],array1))
+    elif(input1=="keys"):
+        encrypt=CryptoLib.encrypt()
     elif(input1.lower()=="exit"):
         break
